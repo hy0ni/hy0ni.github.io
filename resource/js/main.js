1cce1739ffa0db2751cf0e4973164bba4fab400a
hyoni <jih70004@gmail.com>
2023-11-23 22:59:53 +0900

    [add] gnb include 연결

--- resource/js/main.js
+++ resource/js/main.js
@@ -1,11 +1,34 @@
-// 전역 변수
+//*************************************************
+// # 전역 변수
+//*************************************************
 const active = 'is-active';
+const $gnb_load = $('[data-include=gnb]');
 
-(function () {
 
-  /*************************************************
-  * vw setting
-  *************************************************/
+//*************************************************
+// # 문서 로드 후 실행 영역
+//*************************************************
+window.addEventListener('DOMContentLoaded', () => {
+  // ajax load 
+  $.ajaxSetup({
+    method: "GET", // HTTP 요청 메소드(GET, POST 등)
+    dataType: "html" // 서버에서 보내줄 데이터의 타입
+  });
+
+  $.ajax({
+    url: "include/gnb.html", // 클라이언트가 HTTP 요청을 보낼 서버의 URL 주소
+  }).done(html => {
+    $gnb_load.append(html);
+  })
+
+  resizeCheck();
+})
+
+
+//*************************************************
+// # vw setting / 모바일 리사이즈
+//*************************************************
+function resizeCheck() {
 
   let vw = window.innerWidth;
   if (vw > 1280 || vw < 769 && vw > 360) {
@@ -32,4 +55,5 @@ const active = 'is-active';
     });
   }
 
-}());
+}
+
